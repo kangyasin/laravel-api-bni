01b9492b2d1d62175a95f2ddaadcfc4468a17279
kangyasin <tutorialblogmarine@gmail.com>
2022-07-05 13:41:52 +0700

    remove and set key env BNI

--- config/bank-bni.php
+++ config/bank-bni.php
@@ -6,17 +6,17 @@ return [
     /**
      * API Keys
      */
-    'api_key' => env('BNI_API_KEY', '32fa75cd-fd14-46b5-b0b9-3cd253d3028b'),
-    'api_secret' => env('BNI_API_SECRET', '52c443bd-e602-429b-8928-3e2e31232288'),
+    'api_key' => env('BNI_API_KEY', 'YOUR_BNI_API_KEY'),
+    'api_secret' => env('BNI_API_SECRET', 'YOUR_BNI_API_SECRET'),
 
     /**
      * OAuth Credential
      */
-    'client_id' => env('BNI_CLIENT_ID', 'e7617f7e-0a37-4ccd-9993-cfb605744e04'),
-    'client_secret' => env('BNI_CLIENT_SECRET', '2a9609f2-b5e2-4b5c-bcf2-f952609dea3c'),
+    'client_id' => env('BNI_CLIENT_ID', 'YOUR_BNI_CLIENT_ID'),
+    'client_secret' => env('BNI_CLIENT_SECRET', 'YOUR_BNI_CLIENT_SECREET'),
 
     /**
      * Client name is taken from the application name.
      */
-    'client_name' => env('BNI_CLIENT_NAME', 'Govo'),
+    'client_name' => env('BNI_CLIENT_NAME', 'YOUR_BNI_CLIENT_NAME'),
 ];
